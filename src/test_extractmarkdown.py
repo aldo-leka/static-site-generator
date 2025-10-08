3cb1682df7dee72937bb4c89ab3c8eab9d1aa1e2
Aldo Leka <aldo.leka@live.com>
2025-10-08 11:48:01 +0200

    extract markdown img + link with tests

---
+++ src/test_extractmarkdown.py
@@ -0,0 +1,19 @@
+import unittest
+
+from extractmarkdown import extract_markdown_images, extract_markdown_links
+
+class TestExtractMarkdown(unittest.TestCase):
+    def test_extract_markdown_images1(self):
+        matches = extract_markdown_images("This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)")
+        self.assertListEqual(matches, [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")])
+
+    def test_extract_markdown_images2(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links1(self):
+        matches = extract_markdown_links("This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)")
+        self.assertListEqual(matches, [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")])
+
