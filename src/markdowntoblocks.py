271c04c7294e2bfe112a620e36563cb8ac7a6f8d
Aldo Leka <aldo.leka@live.com>
2025-10-08 14:27:33 +0200

    block to blocktype impl w/ tests

--- src/markdowntoblocks.py
+++ src/markdowntoblocks.py
@@ -1,6 +1,63 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     for i in range(0, len(blocks)):
         blocks[i] = blocks[i].strip()
 
     return blocks
+
+def block_to_block_type(block):
+    # Heading
+    for i in range(0, 6):
+        hasj = "#"
+        for j in range(i):
+            hasj += "#"
+
+        if block.startswith(f"{hasj} "):
+            return BlockType.HEADING
+
+    # Code
+    count_backticks = 0
+    for letter in block:
+        if letter == "`":
+            count_backticks += 1
+    if block.startswith("```") and block.endswith("```") and count_backticks >= 6:
+        return BlockType.CODE
+
+    # Quote
+    lines = block.split("\n")
+    quote = True
+    for line in lines:
+        if not line.startswith(">"):
+            quote = False
+    if quote:
+        return BlockType.QUOTE
+
+    # Unordered List
+    unordered_list = True
+    for line in lines:
+        if not line.startswith("- "):
+            unordered_list = False
+    if unordered_list:
+        return BlockType.UNORDERED_LIST
+
+    # Ordered List
+    current = 1
+    ordered_list = True
+    for line in lines:
+        if not line.startswith(f"{current}. "):
+            ordered_list = False
+        current += 1
+    if ordered_list:
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
--- src/test_markdowntoblocks.py
+++ src/test_markdowntoblocks.py
@@ -1,6 +1,7 @@
 import unittest
 
-from markdowntoblocks import markdown_to_blocks
+from markdowntoblocks import markdown_to_blocks, block_to_block_type, BlockType
+
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_markdowntoblocks1(self):
@@ -22,3 +23,36 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_block_type1(self):
+        self.assertEqual(block_to_block_type("###### "), BlockType.HEADING)
+
+    def test_block_to_block_type2(self):
+        self.assertEqual(block_to_block_type("####### "), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type3(self):
+        self.assertEqual(block_to_block_type("# "), BlockType.HEADING)
+
+    def test_block_to_block_type4(self):
+        self.assertEqual(block_to_block_type("```test```"), BlockType.CODE)
+
+    def test_block_to_block_type5(self):
+        self.assertEqual(block_to_block_type("`````"), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type6(self):
+        self.assertEqual(block_to_block_type(">\n>\n>"), BlockType.QUOTE)
+
+    def test_block_to_block_type7(self):
+        self.assertEqual(block_to_block_type(">\n>\n"), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type8(self):
+        self.assertEqual(block_to_block_type("- \n- \n- "), BlockType.UNORDERED_LIST)
+
+    def test_block_to_block_type9(self):
+        self.assertEqual(block_to_block_type("\n- \n- "), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type10(self):
+        self.assertEqual(block_to_block_type("1. \n2. \n3. "), BlockType.ORDERED_LIST)
+
+    def test_block_to_block_type11(self):
+        self.assertEqual(block_to_block_type("2. \n2. \n3. "), BlockType.PARAGRAPH)
